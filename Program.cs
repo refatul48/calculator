0f87ba66d818fc9adf7aaff9074ffdab757598d9
Refatul islam <152413118+refatul48@users.noreply.github.com>
2024-01-31 11:37:30 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace practices
+{
+    class Progaram
+    {
+
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello siam");
+
+            string name, power;
+            Console.WriteLine("###  first Input  ####");
+            name = Console.ReadLine();
+            Console.WriteLine("&&&&&  second input   &&&&&");
+            power = Console.ReadLine();
+            Console.WriteLine("-----program output ----");
+            Console.WriteLine(name + power );
+            Console.WriteLine(name+power+"= hi");
+
+
+            Console.ReadKey();*/
+            /*
+
+            int first_number,second_number;
+            Console.WriteLine("first input");
+            first_number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("second number");
+            second_number = Convert.ToInt32(Console.ReadLine());
+            Console. WriteLine("-------------------------------------");
+            Console.WriteLine("The program output");
+            Console.WriteLine(first_number+second_number);
+            Console.ReadKey();
+            */
+            
+            // Relational oprator (<,>-,==,<=, >=, =!)
+          /*  Console.WriteLine(49>847);
+            Console.WriteLine(84<100);
+            Console.WriteLine(948==984);
+            Console.WriteLine(840<=388);
+            Console.WriteLine(50>=50);
+            Console.WriteLine(10>49 || 50<4 && 50<94 && 94>100 && 40<=89&&94>=74);
+            Console.WriteLine(300>=94);*/
+              
+            int fast_number,second_number,thardnumber,Agread,aplus;
+            second_number=33;
+            thardnumber=40;
+            Agread=60;
+            aplus=80;
+
+            Console.WriteLine("toamr marks input dew");
+            fast_number =Convert.ToInt32(Console.ReadLine());
+            
+            
+            
+           if(fast_number>=second_number)
+        
+           {
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("---tomi passed korcho ok--");
+      
+           }
+           else{
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("tomi fail korco ok-- kosto nio na samne valo kore pora sona korabba tahole passed koraba");
+           }
+           if(fast_number>=thardnumber)
+           {
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("tomi a minus picho");
+
+           }
+           else{
+            
+           }
+           if(fast_number>=Agread)
+           {
+            Console.WriteLine("----------------------------");
+            
+            Console.WriteLine(" A gread picho");
+           }
+            else{
+
+             }
+           if(fast_number>=aplus)
+           {
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("tomi A+ picho,,,,you are welcome dear student");
+           }
+           
+
+
+
+
+            
+
+
+
+        }
+    }
+}
